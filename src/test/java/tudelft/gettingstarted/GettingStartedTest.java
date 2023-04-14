9b4f54e4fa729fb80fa56cd35413f9cc783dc575
Oscar Tran <otran2@uci.edu>
2023-04-13 17:53:37 -0700

    Completed the uncommented tests and added 2 more

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -10,18 +10,30 @@ public class GettingStartedTest {
         int result = new GettingStarted().addFive(20);
         Assertions.assertEquals(25,result);
     }
+    
+    @Test
+    public void addFiveTo21() {
+        int result = new GettingStarted().addFive(21);
+        Assertions.assertEquals(26,result);
+    }
+    
+    @Test
+    public void addFiveTo29() {
+        int result = new GettingStarted().addFive(29);
+        Assertions.assertEquals(34,result);
+    }
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
